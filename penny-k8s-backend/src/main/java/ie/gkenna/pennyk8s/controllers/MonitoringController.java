ceb6bcf3dbd7f8bf8fbf1ca5efd398e4445c04d6
gkenna <thegavinkenna@gmail.com>
2025-04-17 13:21:26 +0000

    Lint fix

--- penny-k8s-backend/src/main/java/ie/gkenna/pennyk8s/controllers/MonitoringController.java
+++ penny-k8s-backend/src/main/java/ie/gkenna/pennyk8s/controllers/MonitoringController.java
@@ -17,49 +17,53 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableScheduling
 public class MonitoringController {
 
-    private static final Logger LOGGER = LogManager.getLogger(MonitoringController.class);
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
-    @Autowired
-    private K8sService k8sService;
+	private static final Logger LOGGER = LogManager.getLogger(MonitoringController.class);
 
-    @PostConstruct
-    public void initWatch() {
-        new Thread(() -> {
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
 
-            while (true) {
-                runWatch("ConfigMap", "/topic/configmaps", () -> k8sService.watchConfigMaps(event -> messagingTemplate
-                        .convertAndSend("/topic/configmaps", new ConfigMapEventDTO(event.type, event.object))));
+	@Autowired
+	private K8sService k8sService;
 
-                runWatch("Pod", "/topic/pods", () -> k8sService.watchPods(event -> messagingTemplate
-                        .convertAndSend("/topic/pods", new PodEventDTO(event.type, event.object))));
+	@PostConstruct
+	public void initWatch() {
+		new Thread(() -> {
 
-                runWatch("Node", "/topic/nodes", () -> k8sService.watchNodes(event -> messagingTemplate
-                        .convertAndSend("/topic/nodes", new NodeEventDTO(event.type, event.object))));
-            }
+			while (true) {
+				runWatch("ConfigMap", "/topic/configmaps", () -> k8sService.watchConfigMaps(event -> messagingTemplate
+					.convertAndSend("/topic/configmaps", new ConfigMapEventDTO(event.type, event.object))));
 
-        }, "k8s-watch-thread").start();
-    }
+				runWatch("Pod", "/topic/pods", () -> k8sService.watchPods(event -> messagingTemplate
+					.convertAndSend("/topic/pods", new PodEventDTO(event.type, event.object))));
 
-    private void runWatch(String resourceType, String topic, Runnable watchTask) {
-        try {
-            LOGGER.info("Starting {} watch...", resourceType);
-            watchTask.run();
-            LOGGER.warn("{} watch exited, restarting...", resourceType);
-        } catch (Exception e) {
-            LOGGER.error("Exception in {} watch thread", resourceType, e);
-        }
+				runWatch("Node", "/topic/nodes", () -> k8sService.watchNodes(event -> messagingTemplate
+					.convertAndSend("/topic/nodes", new NodeEventDTO(event.type, event.object))));
+			}
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException ignored) {
-        }
-    }
+		}, "k8s-watch-thread").start();
+	}
 
-    @Scheduled(fixedRate = 10000)
-    public void publishNodesAndPods() {
-        messagingTemplate.convertAndSend("/topic/nodes", k8sService.getAllNodes());
-        messagingTemplate.convertAndSend("/topic/pods", k8sService.getAllPods());
-    }
+	private void runWatch(String resourceType, String topic, Runnable watchTask) {
+		try {
+			LOGGER.info("Starting {} watch...", resourceType);
+			watchTask.run();
+			LOGGER.warn("{} watch exited, restarting...", resourceType);
+		}
+		catch (Exception e) {
+			LOGGER.error("Exception in {} watch thread", resourceType, e);
+		}
+
+		try {
+			Thread.sleep(3000);
+		}
+		catch (InterruptedException ignored) {
+		}
+	}
+
+	@Scheduled(fixedRate = 10000)
+	public void publishNodesAndPods() {
+		messagingTemplate.convertAndSend("/topic/nodes", k8sService.getAllNodes());
+		messagingTemplate.convertAndSend("/topic/pods", k8sService.getAllPods());
+	}
 
 }
--- penny-k8s-backend/src/main/java/ie/gkenna/pennyk8s/services/K8sService.java
+++ penny-k8s-backend/src/main/java/ie/gkenna/pennyk8s/services/K8sService.java
@@ -27,118 +27,113 @@ import java.util.stream.Collectors;
 @Service
 public class K8sService {
 
-    private static final Logger LOGGER = LogManager.getLogger(K8sService.class);
-    private final CoreV1Api api;
-    private final ApiClient client;
+	private static final Logger LOGGER = LogManager.getLogger(K8sService.class);
 
-    public K8sService() throws IOException {
-        this.client = Config.fromCluster();
-        Configuration.setDefaultApiClient(client);
-        this.api = new CoreV1Api(client);
-    }
+	private final CoreV1Api api;
 
-    public List<NodeInfo> getAllNodes() {
-        try {
-            return api.listNode(null, null, null, null, null, null, null, null, null, null)
-                    .getItems()
-                    .stream()
-                    .map(NodeInfo::fromNode)
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to get nodes", e);
-        }
-    }
+	private final ApiClient client;
 
-    public List<PodInfo> getAllPods() {
-        try {
-            return api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, null)
-                    .getItems()
-                    .stream()
-                    .map(PodInfo::fromPod)
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to get pods", e);
-        }
-    }
+	public K8sService() throws IOException {
+		this.client = Config.fromCluster();
+		Configuration.setDefaultApiClient(client);
+		this.api = new CoreV1Api(client);
+	}
 
-    public List<ConfigMapInfo> getAllConfigMaps() {
-        try {
-            return api.listConfigMapForAllNamespaces(null, null, null, null, null, null, null, null, null, null)
-                    .getItems()
-                    .stream()
-                    .map(ConfigMapInfo::fromConfigMap)
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to get ConfigMaps", e);
-        }
-    }
+	public List<NodeInfo> getAllNodes() {
+		try {
+			return api.listNode(null, null, null, null, null, null, null, null, null, null)
+				.getItems()
+				.stream()
+				.map(NodeInfo::fromNode)
+				.collect(Collectors.toList());
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Failed to get nodes", e);
+		}
+	}
 
-    public String getPodLogs(String namespace, String podName) {
-        try {
-            return api.readNamespacedPodLog(podName, namespace, null, null, null, null, null, null, null, null, null);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to get logs for pod " + podName, e);
-        }
-    }
+	public List<PodInfo> getAllPods() {
+		try {
+			return api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, null)
+				.getItems()
+				.stream()
+				.map(PodInfo::fromPod)
+				.collect(Collectors.toList());
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Failed to get pods", e);
+		}
+	}
 
-    public void watchPods(Consumer<Watch.Response<PodInfo>> onEvent)  {
-        try {
-            watchResources(
-                    api.listPodForAllNamespacesCall(null, null, null, null, null, null, null, null, 60, true, null),
-                    new TypeToken<Watch.Response<V1Pod>>() {},
-                    PodInfo::fromPod,
-                    onEvent,
-                    "Pod"
-            );
-        } catch (ApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public List<ConfigMapInfo> getAllConfigMaps() {
+		try {
+			return api.listConfigMapForAllNamespaces(null, null, null, null, null, null, null, null, null, null)
+				.getItems()
+				.stream()
+				.map(ConfigMapInfo::fromConfigMap)
+				.collect(Collectors.toList());
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Failed to get ConfigMaps", e);
+		}
+	}
 
-    public void watchNodes(Consumer<Watch.Response<NodeInfo>> onEvent) {
-        try {
-            watchResources(
-                    api.listNodeCall(null, null, null, null, null, null, null, null, 60, true, null),
-                    new TypeToken<Watch.Response<V1Node>>() {},
-                    NodeInfo::fromNode,
-                    onEvent,
-                    "Node"
-            );
-        } catch (ApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public String getPodLogs(String namespace, String podName) {
+		try {
+			return api.readNamespacedPodLog(podName, namespace, null, null, null, null, null, null, null, null, null);
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Failed to get logs for pod " + podName, e);
+		}
+	}
 
-    public void watchConfigMaps(Consumer<Watch.Response<ConfigMapInfo>> onEvent)  {
-        try {
-            watchResources(
-                    api.listConfigMapForAllNamespacesCall(null, null, null, null, null, null, null, null, 60, true, null),
-                    new TypeToken<Watch.Response<V1ConfigMap>>() {},
-                    ConfigMapInfo::fromV1ConfigMap,
-                    onEvent,
-                    "ConfigMap"
-            );
-        } catch (ApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public void watchPods(Consumer<Watch.Response<PodInfo>> onEvent) {
+		try {
+			watchResources(
+					api.listPodForAllNamespacesCall(null, null, null, null, null, null, null, null, 60, true, null),
+					new TypeToken<Watch.Response<V1Pod>>() {
+					}, PodInfo::fromPod, onEvent, "Pod");
+		}
+		catch (ApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    private <T, R> void watchResources(
-            Call call,
-            TypeToken<Watch.Response<T>> typeToken,
-            Function<T, R> mapper,
-            Consumer<Watch.Response<R>> onEvent,
-            String resourceName
-    ) {
-        try (Watch<T> watch = Watch.createWatch(client, call, typeToken.getType())) {
-            for (Watch.Response<T> item : watch) {
-                R dto = mapper.apply(item.object);
-                Watch.Response<R> event = new Watch.Response<>(item.type, dto);
-                LOGGER.info("Publishing {} event: {} for {}", resourceName, event.type, dto.toString());
-                onEvent.accept(event);
-            }
-        } catch (Exception e) {
-            LOGGER.error("Error watching {}s", resourceName, e);
-        }
-    }
+	public void watchNodes(Consumer<Watch.Response<NodeInfo>> onEvent) {
+		try {
+			watchResources(api.listNodeCall(null, null, null, null, null, null, null, null, 60, true, null),
+					new TypeToken<Watch.Response<V1Node>>() {
+					}, NodeInfo::fromNode, onEvent, "Node");
+		}
+		catch (ApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void watchConfigMaps(Consumer<Watch.Response<ConfigMapInfo>> onEvent) {
+		try {
+			watchResources(api.listConfigMapForAllNamespacesCall(null, null, null, null, null, null, null, null, 60,
+					true, null), new TypeToken<Watch.Response<V1ConfigMap>>() {
+					}, ConfigMapInfo::fromV1ConfigMap, onEvent, "ConfigMap");
+		}
+		catch (ApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private <T, R> void watchResources(Call call, TypeToken<Watch.Response<T>> typeToken, Function<T, R> mapper,
+			Consumer<Watch.Response<R>> onEvent, String resourceName) {
+		try (Watch<T> watch = Watch.createWatch(client, call, typeToken.getType())) {
+			for (Watch.Response<T> item : watch) {
+				R dto = mapper.apply(item.object);
+				Watch.Response<R> event = new Watch.Response<>(item.type, dto);
+				LOGGER.info("Publishing {} event: {} for {}", resourceName, event.type, dto.toString());
+				onEvent.accept(event);
+			}
+		}
+		catch (Exception e) {
+			LOGGER.error("Error watching {}s", resourceName, e);
+		}
+	}
+
 }
